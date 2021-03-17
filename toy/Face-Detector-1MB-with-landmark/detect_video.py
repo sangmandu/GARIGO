40480730192f234df17112e0f1c05c3c32a71e9c
sangmandu <45033215+sangmandu@users.noreply.github.com>
2021-03-17 17:30:44 +0900

    123
    123

--- toy/Face-Detector-1MB-with-landmark/detect_video.py
+++ toy/Face-Detector-1MB-with-landmark/detect_video.py
@@ -120,7 +120,7 @@ if __name__ == '__main__':
     width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
     height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
 
-    fourcc = cv2.VideoWriter_fourcc(*'XVID')
+    fourcc = cv2.VideoWriter_fourcc(*'MP4V')
     output_movie = cv2.VideoWriter(output_video, fourcc, 29.97, (width, height))
     frame_number = 0
 

3ef2d3986620048506724fb90f3e7c17f3d745ce
sangmandu <45033215+sangmandu@users.noreply.github.com>
2021-03-17 13:19:43 +0900

    detect upgrade version
    detect image
    detect video
    detect video tracking

---
+++ toy/Face-Detector-1MB-with-landmark/detect_video.py
@@ -0,0 +1,266 @@
+from __future__ import print_function
+import os
+import argparse
+import torch
+import torch.backends.cudnn as cudnn
+import numpy as np
+import time
+import face_recognition
+
+from data import cfg_mnet, cfg_slim, cfg_rfb
+from layers.functions.prior_box import PriorBox
+from utils.nms.py_cpu_nms import py_cpu_nms
+import cv2
+from models.retinaface import RetinaFace
+from models.net_slim import Slim
+from models.net_rfb import RFB
+from utils.box_utils import decode, decode_landm
+from utils.timer import Timer
+
+# Create arrays of known face encodings and their names
+# person name : {[file name, encoding]}
+known = {"Yoo Jae Seok": ["jaeseok2.jpg", ], "Jong Kook": ["jongkook.jpg", ]}
+known_face_names, known_face_encodings = [], []
+for person_name in known.keys():
+    print(known[person_name][0])
+    img = face_recognition.load_image_file(known[person_name][0])
+    img_encoding = face_recognition.face_encodings(img)[0]
+    known_face_names.append(person_name)
+    known_face_encodings.append(img_encoding)
+
+# Initialize some variables
+face_locations = []
+face_encodings = []
+face_names = []
+
+parser = argparse.ArgumentParser(description='Test')
+parser.add_argument('-m', '--trained_model', default='./weights/RBF_Final.pth',
+                    type=str, help='Trained state_dict file path to open')
+parser.add_argument('--network', default='RFB', help='Backbone network mobile0.25 or slim or RFB')
+parser.add_argument('--origin_size', default=True, type=str, help='Whether use origin image size to evaluate')
+parser.add_argument('--long_side', default=640, help='when origin_size is false, long_side is scaled size(320 or 640 for long side)')
+parser.add_argument('--save_folder', default='./widerface_evaluate/widerface_txt/', type=str, help='Dir to save txt results')
+parser.add_argument('--cpu', action="store_true", default=True, help='Use cpu inference')
+parser.add_argument('--confidence_threshold', default=0.02, type=float, help='confidence_threshold')
+parser.add_argument('--top_k', default=5000, type=int, help='top_k')
+parser.add_argument('--nms_threshold', default=0.4, type=float, help='nms_threshold')
+parser.add_argument('--keep_top_k', default=750, type=int, help='keep_top_k')
+parser.add_argument('--save_image', action="store_true", default=True, help='show detection results')
+parser.add_argument('--vis_thres', default=0.6, type=float, help='visualization_threshold')
+args = parser.parse_args()
+
+
+def check_keys(model, pretrained_state_dict):
+    ckpt_keys = set(pretrained_state_dict.keys())
+    model_keys = set(model.state_dict().keys())
+    used_pretrained_keys = model_keys & ckpt_keys
+    unused_pretrained_keys = ckpt_keys - model_keys
+    missing_keys = model_keys - ckpt_keys
+    print('Missing keys:{}'.format(len(missing_keys)))
+    print('Unused checkpoint keys:{}'.format(len(unused_pretrained_keys)))
+    print('Used keys:{}'.format(len(used_pretrained_keys)))
+    assert len(used_pretrained_keys) > 0, 'load NONE from pretrained checkpoint'
+    return True
+
+
+def remove_prefix(state_dict, prefix):
+    ''' Old style model is stored with all names of parameters sharing common prefix 'module.' '''
+    print('remove prefix \'{}\''.format(prefix))
+    f = lambda x: x.split(prefix, 1)[-1] if x.startswith(prefix) else x
+    return {f(key): value for key, value in state_dict.items()}
+
+
+def load_model(model, pretrained_path, load_to_cpu):
+    print('Loading pretrained model from {}'.format(pretrained_path))
+    if load_to_cpu:
+        pretrained_dict = torch.load(pretrained_path, map_location=lambda storage, loc: storage)
+    else:
+        device = torch.cuda.current_device()
+        pretrained_dict = torch.load(pretrained_path, map_location=lambda storage, loc: storage.cuda(device))
+    if "state_dict" in pretrained_dict.keys():
+        pretrained_dict = remove_prefix(pretrained_dict['state_dict'], 'module.')
+    else:
+        pretrained_dict = remove_prefix(pretrained_dict, 'module.')
+    check_keys(model, pretrained_dict)
+    model.load_state_dict(pretrained_dict, strict=False)
+    return model
+
+
+if __name__ == '__main__':
+    torch.set_grad_enabled(False)
+
+    cfg = None
+    net = None
+    if args.network == "mobile0.25":
+        cfg = cfg_mnet
+        net = RetinaFace(cfg = cfg, phase = 'test')
+    elif args.network == "slim":
+        cfg = cfg_slim
+        net = Slim(cfg = cfg, phase = 'test')
+    elif args.network == "RFB":
+        cfg = cfg_rfb
+        net = RFB(cfg = cfg, phase = 'test')
+    else:
+        print("Don't support network!")
+        exit(0)
+
+    net = load_model(net, args.trained_model, args.cpu)
+    net.eval()
+    #print('Finished loading model!')
+    #print(net)
+    cudnn.benchmark = True
+    device = torch.device("cpu" if args.cpu else "cuda")
+    net = net.to(device)
+
+    input_video = "clip.mp4"
+    output_video = "output_"+str(args.vis_thres)+"_"+input_video
+
+    cap = cv2.VideoCapture(input_video)
+    length = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+    width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+    height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+
+    fourcc = cv2.VideoWriter_fourcc(*'XVID')
+    output_movie = cv2.VideoWriter(output_video, fourcc, 29.97, (width, height))
+    frame_number = 0
+
+    # testing begin
+    while cap.isOpened():
+        success, image = cap.read()
+        frame_number += 1
+        if not success:
+            break
+
+        #img_raw = cv2.imread(image_path, cv2.IMREAD_COLOR)
+        img_raw = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        img = np.float32(img_raw)
+
+        # testing scale
+        target_size = args.long_side
+        max_size = args.long_side
+        im_shape = img.shape
+        im_size_min = np.min(im_shape[0:2])
+        im_size_max = np.max(im_shape[0:2])
+        resize = float(target_size) / float(im_size_min)
+        # prevent bigger axis from being more than max_size:
+        if np.round(resize * im_size_max) > max_size:
+            resize = float(max_size) / float(im_size_max)
+        if args.origin_size:
+            resize = 1
+
+        if resize != 1:
+            img = cv2.resize(img, None, None, fx=resize, fy=resize, interpolation=cv2.INTER_LINEAR)
+        im_height, im_width, _ = img.shape
+
+
+        scale = torch.Tensor([img.shape[1], img.shape[0], img.shape[1], img.shape[0]])
+        img -= (104, 117, 123)
+        img = img.transpose(2, 0, 1)
+        img = torch.from_numpy(img).unsqueeze(0)
+        img = img.to(device)
+        scale = scale.to(device)
+
+        tic = time.time()
+        loc, conf, landms = net(img)  # forward pass
+        #print('net forward time: {:.4f}'.format(time.time() - tic))
+
+        priorbox = PriorBox(cfg, image_size=(im_height, im_width))
+        priors = priorbox.forward()
+        priors = priors.to(device)
+        prior_data = priors.data
+        boxes = decode(loc.data.squeeze(0), prior_data, cfg['variance'])
+        boxes = boxes * scale / resize
+        boxes = boxes.cpu().numpy()
+        scores = conf.squeeze(0).data.cpu().numpy()[:, 1]
+        #landms = decode_landm(landms.data.squeeze(0), prior_data, cfg['variance'])
+        scale1 = torch.Tensor([img.shape[3], img.shape[2], img.shape[3], img.shape[2],
+                               img.shape[3], img.shape[2], img.shape[3], img.shape[2],
+                               img.shape[3], img.shape[2]])
+        scale1 = scale1.to(device)
+        #landms = landms * scale1 / resize
+        #landms = landms.cpu().numpy()
+
+        # ignore low scores
+        inds = np.where(scores > args.confidence_threshold)[0]
+        boxes = boxes[inds]
+        #landms = landms[inds]
+        scores = scores[inds]
+
+        # keep top-K before NMS
+        order = scores.argsort()[::-1][:args.top_k]
+        boxes = boxes[order]
+        #landms = landms[order]
+        scores = scores[order]
+
+        # do NMS
+        dets = np.hstack((boxes, scores[:, np.newaxis])).astype(np.float32, copy=False)
+        keep = py_cpu_nms(dets, args.nms_threshold)
+        # keep = nms(dets, args.nms_threshold,force_cpu=args.cpu)
+        dets = dets[keep, :]
+        #landms = landms[keep]
+
+        # keep top-K faster NMS
+        dets = dets[:args.keep_top_k, :]
+        #landms = landms[:args.keep_top_k, :]
+
+        #dets = np.concatenate((dets, landms), axis=1)
+
+        # show image
+        if args.save_image:
+            for b in dets:
+                if b[4] < args.vis_thres:
+                    continue
+                text = "{:.4f}".format(b[4])
+                b = list(map(int, b))
+                top, bottom, left, right = b[1], b[3], b[0], b[2]
+
+                face_locations.append((top, right, bottom, left))
+                face_encodings = face_recognition.face_encodings(img_raw, face_locations)
+                face_names = []
+                for face_encoding in face_encodings:
+                    # See if the face is a match for the known face(s)
+                    matches = face_recognition.compare_faces(known_face_encodings, face_encoding)
+                    name = "Unknown"
+
+                    # # If a match was found in known_face_encodings, just use the first one.
+                    # if True in matches:
+                    #     first_match_index = matches.index(True)
+                    #     name = known_face_names[first_match_index]
+
+                    # Or instead, use the known face with the smallest distance to the new face
+                    face_distances = face_recognition.face_distance(known_face_encodings, face_encoding)
+                    best_match_index = np.argmin(face_distances)
+                    if matches[best_match_index]:
+                        name = known_face_names[best_match_index]
+                    face_names.append(name)
+
+                for (top, right, bottom, left), name in zip(face_locations, face_names):
+                    # mosaic
+                    if name == "Unknown":
+                        roi = img_raw[top:bottom, left:right]
+                        ry, rx, _ = roi.shape
+                        if ry <= 15 or rx <= 15:
+                            continue
+                        roi = cv2.resize(roi, (rx // 15, ry // 15))
+                        roi = cv2.resize(roi, (rx, ry), interpolation=cv2.INTER_AREA)
+                        img_raw[top:bottom, left:right] = roi
+
+                    else:
+                        # Draw a box around the face
+                        cv2.rectangle(img_raw, (left, top), (right, bottom), (0, 0, 255), 2)
+                        # Draw a label with a name below the face
+                        cv2.rectangle(img_raw, (left, bottom), (right, bottom + 35), (0, 0, 255), cv2.FILLED)
+                        font = cv2.FONT_HERSHEY_SIMPLEX
+                        cv2.putText(img_raw, name, (left + 6, bottom + 6), font, 1.0, (255, 255, 255), 1)
+
+            img_raw = cv2.cvtColor(img_raw, cv2.COLOR_RGB2BGR)
+            #cv2.imshow('Video', image)
+            print("Writing frame {} / {}".format(frame_number, length))
+            output_movie.write(img_raw)
+            # Hit 'q' on the keyboard to quit!
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+# Release handle to the webcam
+cap.release()
+cv2.destroyAllWindows()
+

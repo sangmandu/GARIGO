0ffecf210f7602ef7df864b24771f6e8913bdb36
lim8540 <lim8540@naver.com>
2021-03-31 17:39:24 +0900

    update isclose.py mosaic.py views.py

--- server/media/views.py
+++ server/media/views.py
@@ -14,8 +14,42 @@ from util.MongoManager import createMedia, getImageByPid
 
 
 class ProfileUploadView(APIView):
-
+    def __init__(self):
+        s3_client = boto3.client(
+                's3',
+                aws_access_key_id="AKIAR26SC7FY7ESCGFCB",
+                aws_secret_access_key="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",
+            )
     def get(self, request):
+        s3_client = boto3.client(
+                's3',
+                aws_access_key_id="AKIAR26SC7FY7ESCGFCB",
+                aws_secret_access_key="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",
+            )
+        root_url = "https://garigo.s3.ap-northeast-2.amazonaws.com/"
+        raw_recog_image_set = getImageByPid('c4a63351-f8b1-45bf-9c7e-ac2934b33d95')
+        recog_list = list(raw_recog_image_set.keys())
+        recog_images = []
+        for name in recog_list:
+            img_urls = []
+            for img_list in raw_recog_image_set[name]:
+                img_url = root_url + img_list["fileUid"]
+                img_urls.append(img_url)
+            recog_images.append(img_urls)
+        input_video = "https://garigo.s3.ap-northeast-2.amazonaws.com/clip.mp4"
+        output_video = mosaic_video(recog_list, recog_images, input_video, "input.mp4")
+        # output_video = "/Users/sumin/project/programmers/GARIGO/server/output_video/output_input.mp4"
+        with open(output_video, 'rb') as data:
+            pid = str(uuid.uuid4())
+            name, extend = output_video.split(".")
+            fileUid = f'{str(uuid.uuid4())}.{extend}'
+            s3_client.put_object(
+                        Body=data,
+                        Bucket="garigo",
+                        Key=fileUid,
+                    )
+        # print()
+
         root_url = "https://garigo.s3.ap-northeast-2.amazonaws.com/"
         raw_recog_image_set = getImageByPid('c4a63351-f8b1-45bf-9c7e-ac2934b33d95')
         recog_list = list(raw_recog_image_set.keys())
@@ -55,3 +89,4 @@ class ProfileUploadView(APIView):
             return JsonResponse({'pid': pid})
         else:
             return JsonResponse({'message': 'file_none'})
+
